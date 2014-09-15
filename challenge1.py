30687a60530cbbb762d8cb0d8057d4c9c23b2b28
Ben Faulkner <thebentelgroup@gmail.com>
2014-09-15 19:47:23 -0400

    Refactored code for Python 3

--- challenge1.py
+++ challenge1.py
@@ -1,11 +1,10 @@
 import base64
+import binascii
 
-myString = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-
+hexString = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+expectedResult = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def hex_str_to_base64(string):
-    return base64.b64encode(string.decode('hex'))
+    return base64.standard_b64encode(binascii.unhexlify(string)).decode('utf-8')
 
-
-assert hex_str_to_base64(myString) == expected
+assert hex_str_to_base64(hexString) == expectedResult
