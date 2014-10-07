5353beb8111ab110e8666153c5986fb6338466ae
Ben Faulkner <thebentelgroup@gmail.com>
2014-10-06 22:46:54 -0400

    Moved unicode decoding back into hex_str_to_base64 function.

--- challenge1.py
+++ challenge1.py
@@ -5,6 +5,6 @@ hexString = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f697
 expectedResult = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def hex_str_to_base64(string):
-    return base64.standard_b64encode(binascii.unhexlify(string))
+    return base64.standard_b64encode(binascii.unhexlify(string)).decode('utf-8')
 
-assert hex_str_to_base64(hexString).decode('utf-8') == expectedResult
+assert hex_str_to_base64(hexString) == expectedResult
