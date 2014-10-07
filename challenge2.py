98fa8bf9ca46124ee138a59dd0c6e50d6eb67990
Ben Faulkner <thebentelgroup@gmail.com>
2014-10-06 22:44:26 -0400

    Initial working version of xor_str function, refactored for Python 3.

--- challenge2.py
+++ challenge2.py
@@ -1,8 +1,19 @@
-a = '1c0111001f010100061a024b53535009181c'
-b = '686974207468652062756c6c277320657965'
-c = '746865206b696420646f6e277420706c6179'
+import binascii
 
-def fixed_xor(a, b):
-    return ''.join([hex(ord(i) ^ ord(j))[2:] for i,j in zip(a.decode('hex'), b.decode('hex'))])
+a = '1c0111001f010100061a024b53535009181c'  # (cypher text)
+b = '686974207468652062756c6c277320657965'  # hit the bull's eye (key)
+c = '746865206b696420646f6e277420706c6179'  # the kid don't play
 
-assert fixed_xor(a, b) == c
+def xor_str(string1, string2):
+    return binascii.hexlify(
+        "".join(
+            chr(ord(leftChr) ^ ord(rightChr))
+            for leftChr, rightChr
+            in zip(
+                binascii.unhexlify(string1.encode('utf-8')).decode('utf-8'),
+                binascii.unhexlify(string2.encode('utf-8')).decode('utf-8')
+            )
+        ).encode('utf-8')
+    ).decode('utf-8')
+
+assert xor_str(a, b) == c
